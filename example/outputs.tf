c49d8fd8c44916734da5ad8520a83e512c05d91a
solairen <michal.oleszek@outlook.com>
2025-02-17 20:15:42 +0000

    Added example

---
+++ example/outputs.tf
@@ -0,0 +1,11 @@
+output "name" {
+  value = module.example_module.name
+}
+
+output "ip" {
+  value = module.example_module.ip
+}
+
+output "ip_single" {
+  value = module.example_module.ip_single
+}
