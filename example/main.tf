4fedc3790fccb585aae833dc2907ce0f66e6d575
solairen <michal.oleszek@outlook.com>
2025-03-04 17:21:06 +0100

    GH-3 [Task]: Update README.md

--- example/main.tf
+++ example/main.tf
@@ -8,19 +8,19 @@ resource "xenorchestra_cloud_config" "cloud_init_config" {
 module "example_module" {
   source = "../module"
 
-  xen_pool_name        = var.pool_name
-  xen_memory_max       = var.memory_size * local.gigabyte_to_byte
-  xen_cpus             = var.cpus
+  xen_pool_name        = "xen-pool"
+  xen_memory_max       = 4294967296 * local.gigabyte_to_byte
+  xen_cpus             = 4
   xen_cloud_config     = xenorchestra_cloud_config.cloud_init_config.template
-  xen_name_label       = var.name_label
+  xen_name_label       = "xen-vm-label"
   xen_name_description = "Example module"
-  xen_template         = var.vm_template
-  xen_network_id       = var.network_adapter
+  xen_template         = "template-name"
+  xen_network_id       = "network-adpater-name"
   xen_expected_ip_cidr = "192.168.0.0/16"
-  xen_disk_sr_id       = var.disk_id
-  xen_disk_name_label  = var.disk_name_label
-  xen_disk_size        = var.disk_size * local.gigabyte_to_byte
+  xen_disk_sr_id       = "disk_id"
+  xen_disk_name_label  = "disk_name_label"
+  xen_disk_size        = 4294967296 * local.gigabyte_to_byte
   xen_clone_type       = "full"
-  xen_tags             = var.tags
-  xen_timeouts         = var.timeouts_create
+  xen_tags             = ["tag1", "tag2"]
+  xen_timeouts         = "20m"
 }
